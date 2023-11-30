50e3e9964af014d934f38dfd218dc6ac57adf098
NicuV23 <v.nicu213@gmail.com>
2023-11-30 15:09:20 +0200

    design

---
+++ index.js
@@ -0,0 +1,20 @@
+
+  const btnRules = document.querySelector(".rules-btn");
+  const btnClose = document.querySelector(".close-btn");
+  const modalRules = document.querySelector(".modal");
+  
+
+  const choiceButtons = document.querySelectorAll(".choice-btn");
+  const gameDiv = document.querySelector(".game");
+  const resultsDiv = document.querySelector(".results");
+  const resultDivs = document.querySelectorAll(".results__result");
+  
+  const resultWinner = document.querySelector(".results__winner");
+  const resultText = document.querySelector(".results__text");
+  
+  const playAgainBtn = document.querySelector(".play-again");
+  
+  const scoreNumber = document.querySelector(".score__number");
+  let score = 0;
+  
+  
